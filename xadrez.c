49d65326c2dc7267eeec84cbd4933982a787064f
Bit.Raiden <59292183+MiguelQueiroz010@users.noreply.github.com>
2025-11-25 13:31:41 +0000

    intermed

--- xadrez.c
+++ xadrez.c
@@ -36,10 +36,22 @@ int main() {
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
+    int f = 3;
+    while(f > 1)
+    {
+        for(int i = 0; i < 2; i++, f--)
+        {
+            printf("O cavalo moveu-se para baixo ↓!\n");
+        }
+        printf("O cavalo moveu-se para a esquerda ←!\n");
+    }
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    printf("\nMODO MESTRE ATIVADO!\n");
+
+    
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
